61faa42de51a48da30065c95d3089db3e698bd42
keviniporre <keviniporres@gmail.com>
2023-04-27 01:12:37 -0400

    Created Verify / Add Method
    Created the verify empty/full queue method and the add item method.

--- FinalProject_DSA_UTEPSA/queueClass.cs
+++ FinalProject_DSA_UTEPSA/queueClass.cs
@@ -8,9 +8,49 @@ namespace FinalProject_DSA_UTEPSA
 {
     public class queueClass:valueClass
     {
+        //Declaracion de variable/atributos
+        private int first, last, maxLength;
+        private int[] queueContainer;
+
+        //Declaracion Constructor
+        public queueClass (int maxLength)
+        {
+            this.first = 0;
+            this.last = -1;
+            this.maxLength = maxLength;
+            this.queueContainer = new int[maxLength];
+        }
+        //Declaracion Metodos Verificadores - True/False
+        public bool isQueueEmpty()
+        {
+            if (this.last < this.first) { return true; }
+            else { return false; }
+        }
+        public bool isQueueFull()
+        {
+            if (this.last == (maxLength - 1) && (this.first == 0) ) { return true; }
+            else { return false; }
+        }
+        //Declaracion Metodos Ejecucion en Cola
+        public void insertItem(int item)
+        {
+            if (this.isQueueFull()) { MessageBox.Show("The Queue is Full!"); }
+            else
+            {
+                this.last++;
+                this.queueContainer[last] = item;
+                MessageBox.Show("The Item was inserted to the Queue");
+            }
+        }
+        public void deleteItem()
+        {
+            if (this.isQueueEmpty()) { MessageBox.Show("The Queue is Empty!"); }
+            else { }
+        }
         public void clearBoxes(TextBox textBox)
         {
             textBox.Text = "";
         }
+
     }
 }
