d19273b87b3b3b5bde2a694f906bab5f8b821e87
keviniporre <keviniporres@gmail.com>
2023-04-26 23:54:30 -0400

    Updated GUI
    Updated the GUI for the app

--- FinalProject_DSA_UTEPSA/Form1.Designer.cs
+++ FinalProject_DSA_UTEPSA/Form1.Designer.cs
@@ -43,13 +43,18 @@
             label7 = new Label();
             dataGridView1 = new DataGridView();
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            dataGridView2 = new DataGridView();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(130, 23);
+            label1.Location = new Point(52, 21);
             label1.Name = "label1";
             label1.Size = new Size(87, 15);
             label1.TabIndex = 0;
@@ -64,35 +69,35 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(136, 87);
+            textBox2.Location = new Point(136, 85);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(100, 23);
             textBox2.TabIndex = 2;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(136, 116);
+            textBox3.Location = new Point(136, 114);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(100, 23);
             textBox3.TabIndex = 3;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(136, 145);
+            textBox4.Location = new Point(136, 143);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(100, 23);
             textBox4.TabIndex = 4;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(136, 190);
+            textBox5.Location = new Point(136, 172);
             textBox5.Name = "textBox5";
             textBox5.Size = new Size(100, 23);
             textBox5.TabIndex = 5;
             // 
             // textBox6
             // 
-            textBox6.Location = new Point(136, 230);
+            textBox6.Location = new Point(136, 201);
             textBox6.Name = "textBox6";
             textBox6.Size = new Size(100, 23);
             textBox6.TabIndex = 6;
@@ -100,61 +105,61 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(52, 59);
+            label2.Location = new Point(53, 59);
             label2.Name = "label2";
-            label2.Size = new Size(38, 15);
+            label2.Size = new Size(21, 15);
             label2.TabIndex = 7;
-            label2.Text = "label2";
+            label2.Text = "ID:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(52, 90);
+            label3.Location = new Point(52, 88);
             label3.Name = "label3";
-            label3.Size = new Size(38, 15);
+            label3.Size = new Size(42, 15);
             label3.TabIndex = 8;
-            label3.Text = "label3";
+            label3.Text = "Name:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(52, 116);
+            label4.Location = new Point(53, 117);
             label4.Name = "label4";
-            label4.Size = new Size(38, 15);
+            label4.Size = new Size(61, 15);
             label4.TabIndex = 9;
-            label4.Text = "label4";
+            label4.Text = "Instructor:";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(52, 153);
+            label5.Location = new Point(53, 146);
             label5.Name = "label5";
-            label5.Size = new Size(38, 15);
+            label5.Size = new Size(58, 15);
             label5.TabIndex = 10;
-            label5.Text = "label5";
+            label5.Text = "Category:";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(52, 198);
+            label6.Location = new Point(53, 175);
             label6.Name = "label6";
-            label6.Size = new Size(38, 15);
+            label6.Size = new Size(47, 15);
             label6.TabIndex = 11;
-            label6.Text = "label6";
+            label6.Text = "Length:";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(52, 238);
+            label7.Location = new Point(52, 204);
             label7.Name = "label7";
-            label7.Size = new Size(38, 15);
+            label7.Size = new Size(62, 15);
             label7.TabIndex = 12;
-            label7.Text = "label7";
+            label7.Text = "Language:";
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(52, 279);
+            dataGridView1.Location = new Point(53, 242);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowTemplate.Height = 25;
             dataGridView1.Size = new Size(308, 147);
@@ -162,18 +167,58 @@
             // 
             // button1
             // 
-            button1.Location = new Point(285, 59);
+            button1.Location = new Point(285, 55);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 14;
             button1.Text = "Add";
             button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            button2.Location = new Point(285, 84);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 15;
+            button2.Text = "Delete";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(285, 113);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 16;
+            button3.Text = "Clear";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(392, 204);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 17;
+            button4.Text = "Search";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(392, 242);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowTemplate.Height = 25;
+            dataGridView2.Size = new Size(308, 147);
+            dataGridView2.TabIndex = 18;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(432, 450);
+            ClientSize = new Size(749, 413);
+            Controls.Add(dataGridView2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(dataGridView1);
             Controls.Add(label7);
@@ -192,6 +237,7 @@
             Name = "Form1";
             Text = "Online Courses";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -213,5 +259,9 @@
         private Label label7;
         private DataGridView dataGridView1;
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private DataGridView dataGridView2;
     }
 }
